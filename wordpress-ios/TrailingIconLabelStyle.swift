d2cdb6ada6d22a0da3a3d461347f2d498fb8533a
ZHOU <510878689@qq.com>
2022-08-20 20:19:53 +0800

    feat:training/creating-a-card-view

---
+++ wordpress-ios/TrailingIconLabelStyle.swift
@@ -0,0 +1,21 @@
+//
+//  TrailingIconLabelStyle.swift
+//  wordpress-ios
+//
+//  Created by cafehaus on 2022/8/20.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
