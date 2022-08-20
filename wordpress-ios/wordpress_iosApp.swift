49be3053a45e96f3b6e94919059a9ef7a4d35744
ZHOU <510878689@qq.com>
2022-08-20 12:01:12 +0800

    feat:inital project

---
+++ wordpress-ios/wordpress_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordpress_iosApp.swift
+//  wordpress-ios
+//
+//  Created by cafehaus on 2022/8/20.
+//
+
+import SwiftUI
+
+@main
+struct wordpress_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ wordpress-iosUITests/wordpress_iosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  wordpress_iosUITestsLaunchTests.swift
+//  wordpress-iosUITests
+//
+//  Created by cafehaus on 2022/8/20.
+//
+
+import XCTest
+
+class wordpress_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
